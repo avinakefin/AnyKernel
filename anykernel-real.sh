9a3f120841f41e56742116ff0a387ef08365fbed
avinakefin <ojiappaijo@gmail.com>
2022-12-12 20:08:39 +0700

    fixed slot

--- anykernel-real.sh
+++ anykernel-real.sh
@@ -4,27 +4,23 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
-kernel.for=KernelForUnknown
-kernel.compiler=none
-kernel.made=Anu
-kernel.version=4.4.xxx
-message.word=dofoljaran
-build.date=2077
+kernel.string=Boot dan Dtbo sudah di backup ?
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=alioth
-device.name2=kona
-supported.versions=9.0-13.0
-supported.patchlevels=
+device.name2=aliothin
+device.name3=
+device.name4=
+device.name5=
+supported.versions=
 '; } # end properties
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=0;
+is_slot_device=1;
 ramdisk_compression=auto;
 
 
@@ -35,75 +31,18 @@ ramdisk_compression=auto;
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+set_perm_recursive 0 0 750 750 $ramdisk/*;
 
 
 ## AnyKernel install
 dump_boot;
 
-# begin ramdisk changes
+# Begin Ramdisk Changes
 
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
-
-# remove spectrum profile
-if [ -e $ramdisk/init.spectrum.rc ];then
-  rm -rf $ramdisk/init.spectrum.rc
-  ui_print "delete /init.spectrum.rc"
-fi
-if [ -e $ramdisk/init.spectrum.sh ];then
-  rm -rf $ramdisk/init.spectrum.sh
-  ui_print "delete /init.spectrum.sh"
-fi
-if [ -e $ramdisk/sbin/init.spectrum.rc ];then
-  rm -rf $ramdisk/sbin/init.spectrum.rc
-  ui_print "delete /sbin/init.spectrum.rc"
-fi
-if [ -e $ramdisk/sbin/init.spectrum.sh ];then
-  rm -rf $ramdisk/sbin/init.spectrum.sh
-  ui_print "delete /sbin/init.spectrum.sh"
-fi
-if [ -e $ramdisk/etc/init.spectrum.rc ];then
-  rm -rf $ramdisk/etc/init.spectrum.rc
-  ui_print "delete /etc/init.spectrum.rc"
-fi
-if [ -e $ramdisk/etc/init.spectrum.sh ];then
-  rm -rf $ramdisk/etc/init.spectrum.sh
-  ui_print "delete /etc/init.spectrum.sh"
-fi
-if [ -e $ramdisk/init.aurora.rc ];then
-  rm -rf $ramdisk/init.aurora.rc
-  ui_print "delete /init.aurora.rc"
-fi
-if [ -e $ramdisk/sbin/init.aurora.rc ];then
-  rm -rf $ramdisk/sbin/init.aurora.rc
-  ui_print "delete /sbin/init.aurora.rc"
-fi
-if [ -e $ramdisk/etc/init.aurora.rc ];then
-  rm -rf $ramdisk/etc/init.aurora.rc
-  ui_print "delete /etc/init.aurora.rc"
-fi
-
-# patch android version
-android_ver=$(file_getprop /system/build.prop ro.build.version.release);
-patch_cmdline androidboot.version androidboot.version=android_ver
-
-# end ramdisk changes
+# migrate from /overlay to /overlay.d to enable SAR Magisk
+if [ -d $ramdisk/overlay ]; then
+  rm -rf $ramdisk/overlay;
+fi;
 
 write_boot;
 ## end install
-
